ce1d911cd0471a66dda5b6b1210ff5ce2d4fef19
Max <maximpely@gmail.com>
2018-03-15 22:01:56 +0000

    .

---
+++ pi/pi.py
@@ -0,0 +1,15 @@
+from random import uniform
+from console import clear
+r = 1
+
+n = 0
+inside = 0
+while True:
+	for _ in range(100000):
+		n += 1
+		point = (uniform(-r,r), uniform(-r,r))
+		if point[0]**2 + point[1]**2 < 1:
+			inside += 1
+	pi = inside * (2*r)**2 / n
+	clear()
+	print(pi)
